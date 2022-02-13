2e88617155710bd77dbc52a3a68155c6895caa68
Nick-Telles <nicktelles78@gmail.com>
2022-02-13 13:01:03 -0500

    f

--- src/LocationFromIP.js
+++ src/LocationFromIP.js
@@ -1,74 +1,74 @@
-// dependencies / things imported
-import { LitElement, html, css } from 'lit';
-import { UserIP } from './UserIP.js';
+// // dependencies / things imported
+// import { LitElement, html, css } from 'lit';
+// import { UserIP } from './UserIP.js';
 
-export class LocationFromIP extends LitElement {
-  static get tag() {
-    return 'location-from-ip';
-  }
+// export class LocationFromIP extends LitElement {
+//   static get tag() {
+//     return 'location-from-ip';
+//   }
 
-  constructor() {
-    super();
-    this.UserIpInstance = new UserIP();
-    this.locationEndpoint = 'https://freegeoip.app/json/';
-    this.long = null;
-    this.lat = null;
-  }
+//   constructor() {
+//     super();
+//     this.UserIpInstance = new UserIP();
+//     this.locationEndpoint = 'https://freegeoip.app/json/';
+//     this.long = null;
+//     this.lat = null;
+//   }
 
-  static get properties() {
-    return {
-      lat: {type: Number, reflect: true},
-      long: {type: Number, reflect: true},
-    };
+//   static get properties() {
+//     return {
+//       lat: {type: Number, reflect: true},
+//       long: {type: Number, reflect: true},
+//     };
       
-  }
+//   }
 
-  firstUpdated(changedProperties) {
-    if (super.firstUpdated) {
-      super.firstUpdated(changedProperties);
-    }
-    this.getGEOIPData();
-  }
+//   firstUpdated(changedProperties) {
+//     if (super.firstUpdated) {
+//       super.firstUpdated(changedProperties);
+//     }
+//     this.getGEOIPData();
+//   }
 
-  async getGEOIPData() {
-    const IPClass = new UserIP();
-    const userIPData = await IPClass.updateUserIP();
-    return fetch(this.locationEndpoint + userIPData.ip)
-      .then(resp => {
-        if (resp.ok) {
-          return resp.json();
-        }
-        return false;
-      })
-      .then(data => {
-        console.log(data);
-        this.long = data.longitude;
-        this.lat = data.latitude;
-        return data;
-      });
-  }
+//   async getGEOIPData() {
+//     const IPClass = new UserIP();
+//     const userIPData = await IPClass.updateUserIP();
+//     return fetch(this.locationEndpoint + userIPData.ip)
+//       .then(resp => {
+//         if (resp.ok) {
+//           return resp.json();
+//         }
+//         return false;
+//       })
+//       .then(data => {
+//         console.log(data);
+//         this.long = data.longitude;
+//         this.lat = data.latitude;
+//         return data;
+//       });
+//   }
 
-  static get styles() {
-    return [
-      css`
-        :host {
-          display: block;
-        }
-        iframe {
-          height: 500px;
-          width: 500px;
-        }
-      `,
-    ];
-  }
+//   static get styles() {
+//     return [
+//       css`
+//         :host {
+//           display: block;
+//         }
+//         iframe {
+//           height: 500px;
+//           width: 500px;
+//         }
+//       `,
+//     ];
+//   }
 
-  render() {
-    // this function runs every time a properties() declared variable changes
-    // this means you can make new variables and then bind them this way if you like
-    console.log(`lat: ${this.lat} long ${this.long}`);
-    const url = `https://maps.google.com/maps?q=${this.long},${this.lat}&t=&z=15&ie=UTF8&iwloc=&output=embed`;
-    return html`<iframe title="Where you are" src="${url}"></iframe> `;
-  }
-}
+//   render() {
+//     // this function runs every time a properties() declared variable changes
+//     // this means you can make new variables and then bind them this way if you like
+//     console.log(`lat: ${this.lat} long ${this.long}`);
+//     const url = `https://maps.google.com/maps?q=${this.long},${this.lat}&t=&z=15&ie=UTF8&iwloc=&output=embed`;
+//     return html`<iframe title="Where you are" src="${url}"></iframe> `;
+//   }
+// }
 
-customElements.define(LocationFromIP.tag, LocationFromIP);
+// customElements.define(LocationFromIP.tag, LocationFromIP);
--- src/TimeTill.js
+++ src/TimeTill.js
@@ -1,34 +1,34 @@
-// dependencies / things imported
-import { LitElement, html, css } from 'lit';
-import { UserIP } from './UserIP.js';
+// // dependencies / things imported
+// import { LitElement, html, css } from 'lit';
+// import { UserIP } from './UserIP.js';
 
-export class TimeTill extends LitElement {
-  static get tag() {
-    return 'time-till';
-  }
+// export class TimeTill extends LitElement {
+//   static get tag() {
+//     return 'time-till';
+//   }
 
-  constructor() {
-    super();
-    this.UserIpInstance = new UserIP();
-  }
+//   constructor() {
+//     super();
+//     this.UserIpInstance = new UserIP();
+//   }
 
-  static get properties() {
-    return {};
-  }
+//   static get properties() {
+//     return {};
+//   }
 
-  static get styles() {
-    return [
-      css`
-        :host {
-          display: block;
-        }
-      `,
-    ];
-  }
+//   static get styles() {
+//     return [
+//       css`
+//         :host {
+//           display: block;
+//         }
+//       `,
+//     ];
+//   }
 
-  render() {
-    return html`<h1>Hello</h1>`;
-  }
-}
+//   render() {
+//     return html`<h1>Hello</h1>`;
+//   }
+// }
 
-customElements.define(TimeTill.tag, TimeTill);
+// customElements.define(TimeTill.tag, TimeTill);
